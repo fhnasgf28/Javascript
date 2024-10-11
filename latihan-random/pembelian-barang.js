7a88c165fdae8e4fb56badb4e2bcdca4eed2410e
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-10-12 06:31:29 +0700

    latihan javascript random

---
+++ latihan-random/pembelian-barang.js
@@ -0,0 +1,29 @@
+function calculateTotalCost(itemType, price, quantity) {
+	let totalCost = 0;
+	let discount = 0;
+
+	// calculate discount based on item type and quantity
+	switch (itemType) {
+		case "book":
+			if (quantity >= 5) {
+				discount = 0.10;
+			}
+			break;
+		case "electronic":
+			if (quantity >= 3) {
+				discount = 0.05;
+			}
+		case "clothing":
+			discount = 0;
+			break;
+		default:
+			console.error(`invalid item type: {itemType}`);
+			return null;
+	}
+
+	// calculate total cost
+	totalCost = price * quantity;
+	totalCost -= totalCost * discount;
+
+	return totalCost;
+}
