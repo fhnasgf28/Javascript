95c94020e974ae9ef57f9e58e16d762cb7b1994c
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-10-18 07:42:09 +0700

    menambahkan file javascript

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -1,3 +1,43 @@
+const { link } = require("fs");
+
 // select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+// close link 
+const navToggle = document.querySelector(".nav-toggle");
+const LinksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    // linksContainer.classList.toggle("show-links");
+    const containerHeight = LinksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+        links.style.height = `${links.scrollHeight}px`;
+    } else {
+        LinksContainer.style.height = 0;
+    }
+    console.log(LinksContainer.getBoundingClientRect());
+});
+
+// fixed navbar
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+    // setup back to top link
+    if (scrollHeight > 500) {
+        console.log("hello");
+
+        topLink.classList.add("show-link");
+    } else {
+        topLink.classList.remove("show-link");
+    }
+})
