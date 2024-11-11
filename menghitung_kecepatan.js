46691f64e03c0b15d6b519373175dbcf45c79027
Farhan Assegaf <82705668+fhnasgf28@users.noreply.github.com>
2024-11-11 21:40:16 +0700

    Create menghitung_kecepatan.js

---
+++ menghitung_kecepatan.js
@@ -0,0 +1,10 @@
+// Jarak yang akan ditempuh dalam kilometer
+const distance = 200;
+
+// Kecepatan rata-rata dalam kilometer per jam
+const averageSpeed = 60;
+
+// Menghitung waktu yang dibutuhkan dalam jam
+const time = distance / averageSpeed;
+
+console.log(`Waktu yang dibutuhkan untuk menempuh jarak ${distance} km dengan kecepatan rata-rata ${averageSpeed} km/jam adalah ${time} jam.`);
