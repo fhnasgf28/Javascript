78a9f676de69f8a39c49c5147d4efe354ce00899
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-05-18 22:00:10 +0700

    belajar nextjs 15

---
+++ React-js/next-portfolio-template/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+import { ThemeProvider } from 'next-themes'
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio Template",
+  description: "Created by Vinodjangid.site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
