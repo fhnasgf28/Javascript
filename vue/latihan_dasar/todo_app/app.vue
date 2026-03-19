63afa5df564af940f72555e83601b86df5168992
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2026-03-19 16:57:29 +0700

    menambahkan latihan vue

---
+++ vue/latihan_dasar/todo_app/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="app-container">
+        <header class="header">
+            <h1>Todo App</h1>
+            <p>Manage your tasks efficiently and stay organized with our user-friendly Todo App.</p>
+        </header>
+
+        <main class="main-content">
+            <section class="input-section">
+                
+            </section>
+        </main>
+    </div>
+</template>
